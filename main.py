001f8e786d410f4f83e5d01fab00a8da64eb6510
aronvanya <aronvanya@gmail.com>
2025-01-23 12:02:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,33 +5,29 @@ from telethon.sessions import StringSession
 import threading
 
 # Настройки API
-api_id = 21078867  # Ваш API ID
-api_hash = "95e80b6bea78c5b0c5442702c8cc17de"  # Ваш API Hash
+api_id = 21078867
+api_hash = "95e80b6bea78c5b0c5442702c8cc17de"
 string_session = "1ApWapzMBu480WTeHnPyr_MsiPbeabG6UVEHJr67wOp6PYv1em6paWIKpbVNO4QY-eGnI3T_IplUyK7QzZs31nhLy-neLeaQeSy39kBUWKBCSECjN78KjPJz7g9d9R1YMELLCkx4_cpPC41HQQJPIa2jUQTZV0LlRNN3EyOVh3G_ouvW_AUhW1kd-dw49xzV4Opz9GdvAwlFgVYkBrSS6wYDW1T4XlmJdGDw2G-Vwfw34_-2T1xx0CXybl1pnrmVXmfJxepwegQXZ1NLjBYF75tS7ioa1oB-YR7RWyiwEcPMuGdM0lBJEIjiT4ncX_WBzeq4WkWxuAM0VlduuQ9YcoGW3nT4ikDw="
 
 # Данные каналов
-source_channel_id = -1002361161091  # ID канала-источника
-target_channel_id = -1002324576765  # ID целевой группы
-
-# ID разделов, которые нужно пересылать
+source_channel_id = -1002361161091
+target_channel_id = -1002324576765
 allowed_topics = [3, 5, 6, 976, 1986, 736]
 
-# Инициализация Telegram клиента
+# Инициализация клиента
 client = TelegramClient(StringSession(string_session), api_id, api_hash)
 
-# Настройка Flask
+# Flask-сервер
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Bot is running!"
 
-# Обработчик новых сообщений
 @client.on(events.NewMessage(chats=source_channel_id))
 async def handler(event):
     message = event.message
     reply_to = message.reply_to
-
     if reply_to:
         topic_id = reply_to.reply_to_top_id if reply_to.reply_to_top_id else reply_to.reply_to_msg_id
         if topic_id in allowed_topics:
@@ -47,7 +43,7 @@ async def handler(event):
 
 # Запуск Flask в отдельном потоке
 def run_flask():
-    port = int(os.environ.get("PORT", 8080))  # Используем порт, предоставленный Heroku
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 flask_thread = threading.Thread(target=run_flask)
