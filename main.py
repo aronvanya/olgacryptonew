479335cd56c37f96245033fbcfedb8bdbcb1cb64
aronvanya <aronvanya@gmail.com>
2025-01-24 11:32:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ api_hash = "95e80b6bea78c5b0c5442702c8cc17de"
 string_session = "1ApWapzMBu3jCir6F04lSI2Rhzm2hDITwY_z3lNH1jsj1OfwUFVQEEVAjaZcSAv2yPl4FgE3mdWHCvk8SOm8HlG25OHzhAhjd79IrEQA66tPfBSisXnfmxAZyCTvzKkeL9NV44JaG96mVaK1VQG1HujvJp-8yxGHYc4BTyFD_ANr0SnsNEka1ZnXDshX4VRcwAAXd3GFV4wupzo0mhQJ2htWkbHXgNKHk6nW_hUkikbGr0w_m5tgRxXRBbnBBLLi1-p8W87OOJYrEdblWlUEMTZJmLk19LQ466nEgg58dCFlMqszwx9lmB-RHoYsJyGEj1Ol3714OaObFNBlkkyvWqd0ESCxNkWI="
 
 # Данные каналов
-source_channel_id = -1002361161091
-target_channel_id = -1002324576765
+source_channel_id = -1002361161091  # ID группы-источника
+target_channel_id = -1002324576765  # ID целевой группы
 
 # Соответствие разделов
 section_mapping = {
@@ -41,19 +41,23 @@ async def handler(event):
         message = event.message
         reply_to = message.reply_to
 
-        print(f"Новое сообщение: {message.text}")
         if reply_to:
             topic_id = reply_to.reply_to_top_id if reply_to.reply_to_top_id else reply_to.reply_to_msg_id
             if topic_id in section_mapping:
                 target_topic = section_mapping[topic_id]
-                await client.send_message(target_channel_id, message.text)
+                await client.send_message(target_channel_id, message.text, reply_to=target_topic)
                 print(f"Сообщение из раздела {topic_id} отправлено в раздел {target_topic}: {message.text}")
             else:
                 print(f"Пропущено: сообщение из раздела {topic_id}")
         else:
             print(f"Пропущено: сообщение без reply_to. Полное сообщение: {message.to_dict()}")
     except Exception as e:
-        print(f"Ошибка в обработчике сообщений: {e}")
+        print(f"Ошибка при обработке сообщения: {e}")
+
+# Дебаг-обработчик для всех новых сообщений
+@client.on(events.NewMessage)
+async def debug_handler(event):
+    print(f"Новое сообщение из чата {event.chat_id}: {event.message.text}")
 
 # Функция для самопинга
 def ping_self():
